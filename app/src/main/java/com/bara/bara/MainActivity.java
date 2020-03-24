5ff4597491537d0538ab1cff080f532e82968e06
Ovidiu Taraleșca <taralesca@users.noreply.github.com>
2020-03-24 20:29:24 +0200

    [BARA-003] Change models on button tap

--- app/src/main/java/com/bara/bara/MainActivity.java
+++ app/src/main/java/com/bara/bara/MainActivity.java
@@ -3,11 +3,13 @@ package com.bara.bara;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.ar.core.ArCoreApk;
 import com.google.ar.core.AugmentedFace;
 import com.google.ar.core.TrackingState;
@@ -16,28 +18,24 @@ import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.Renderable;
-import com.google.ar.sceneform.rendering.Texture;
 import com.google.ar.sceneform.ux.AugmentedFaceNode;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
 
-    private FaceArFragment arFragment;
+    private List<ModelRenderable> models = new ArrayList<>();
+    private boolean changeModel = false;
+    private int modelIndex = 0;
+
     private ModelRenderable faceRegionsRenderable;
-//    private Texture faceMeshTexture;
     private final HashMap<AugmentedFace, AugmentedFaceNode> faceNodeMap = new HashMap<>();
 
     @Override
@@ -49,56 +47,77 @@ public class MainActivity extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_main);
-        arFragment = (FaceArFragment) getSupportFragmentManager().findFragmentById(R.id.face_fragment);
 
+        ImageButton btn = findViewById(R.id.button_next);
+        btn.setOnClickListener(v -> {
+            changeModel = !changeModel;
+            modelIndex++;
+            if (modelIndex > models.size() - 1) {
+                modelIndex = 0;
+            }
+            faceRegionsRenderable = models.get(modelIndex);
+        });
+
+        FaceArFragment arFragment = (FaceArFragment) getSupportFragmentManager().findFragmentById(R.id.face_fragment);
+        buildModel(R.raw.hors);
+        buildModel(R.raw.glasses);
+
+        final ArSceneView sceneView = arFragment.getArSceneView();
+        sceneView.setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
+        final Scene scene = sceneView.getScene();
+
+        scene.addOnUpdateListener(
+                (FrameTime frameTime) -> {
+                    if (faceRegionsRenderable == null) {// || faceMeshTexture == null) {
+                        return;
+                    }
+                    final Collection<AugmentedFace> faceList = sceneView.getSession()
+                            .getAllTrackables(AugmentedFace.class);
+                    // Make new AugmentedFaceNodes for any new faces.
+                    makeAugumentedFaceNodes(scene, faceList);
+                    changeModel = false;
+                    // Remove any AugmentedFaceNodes associated with an AugmentedFace that stopped tracking.
+                    removeAugumentedFaceNodes();
+                });
+    }
+
+    private void removeAugumentedFaceNodes() {
+        final Iterator<Map.Entry<AugmentedFace, AugmentedFaceNode>> iter = faceNodeMap.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<AugmentedFace, AugmentedFaceNode> entry = iter.next();
+            AugmentedFace face = entry.getKey();
+            if (face.getTrackingState() == TrackingState.STOPPED) {
+                AugmentedFaceNode faceNode = entry.getValue();
+                faceNode.setParent(null);
+                iter.remove();
+            }
+        }
+    }
+
+    private void makeAugumentedFaceNodes(Scene scene, Collection<AugmentedFace> faceList) {
+        for (AugmentedFace face : faceList) {
+            if (!faceNodeMap.containsKey(face)) {
+                AugmentedFaceNode faceNode = new AugmentedFaceNode(face);
+                faceNode.setParent(scene);
+                faceNode.setFaceRegionsRenderable(faceRegionsRenderable);
+                faceNodeMap.put(face, faceNode);
+            } else if (changeModel) {
+                faceNodeMap.get(face).setFaceRegionsRenderable(faceRegionsRenderable);
+            }
+        }
+    }
+
+    private void buildModel(int modelId) {
         ModelRenderable.builder()
-                .setSource(this, R.raw.hors)
+                .setSource(this, modelId)
                 .build()
                 .thenAccept(
                         modelRenderable -> {
+                            models.add(modelRenderable);
                             faceRegionsRenderable = modelRenderable;
                             modelRenderable.setShadowCaster(false);
                             modelRenderable.setShadowReceiver(false);
                         });
-
-        ArSceneView sceneView = arFragment.getArSceneView();
-        sceneView.setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
-        Scene scene = sceneView.getScene();
-
-        scene.addOnUpdateListener(
-            (FrameTime frameTime) -> {
-                if (faceRegionsRenderable == null) {// || faceMeshTexture == null) {
-                    return;
-                }
-
-                Collection<AugmentedFace> faceList =
-                        sceneView.getSession().getAllTrackables(AugmentedFace.class);
-
-                // Make new AugmentedFaceNodes for any new faces.
-                for (AugmentedFace face : faceList) {
-                    if (!faceNodeMap.containsKey(face)) {
-                        AugmentedFaceNode faceNode = new AugmentedFaceNode(face);
-                        faceNode.setParent(scene);
-                        faceNode.setFaceRegionsRenderable(faceRegionsRenderable);
-//                        faceNode.setFaceMeshTexture(faceMeshTexture);
-                        faceNodeMap.put(face, faceNode);
-                    }
-                }
-
-                // Remove any AugmentedFaceNodes associated with an AugmentedFace that stopped tracking.
-                Iterator<Map.Entry<AugmentedFace, AugmentedFaceNode>> iter =
-                        faceNodeMap.entrySet().iterator();
-                while (iter.hasNext()) {
-                    Map.Entry<AugmentedFace, AugmentedFaceNode> entry = iter.next();
-                    AugmentedFace face = entry.getKey();
-                    if (face.getTrackingState() == TrackingState.STOPPED) {
-                        AugmentedFaceNode faceNode = entry.getValue();
-                        faceNode.setParent(null);
-                        iter.remove();
-                    }
-                }
-            });
-
     }
 
     public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
