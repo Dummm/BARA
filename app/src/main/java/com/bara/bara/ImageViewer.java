0a545e20a6816f47f9e1f498aadd11af071def73
taralesca <ovidiu-costinel.taralesca@my.fmi.unibuc.ro>
2020-05-29 17:13:28 +0300

    Add buttons for save, delete and share

--- app/src/main/java/com/bara/bara/ImageViewer.java
+++ app/src/main/java/com/bara/bara/ImageViewer.java
@@ -2,14 +2,17 @@ package com.bara.bara;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ImageViewer extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +25,30 @@ public class ImageViewer extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageURI(imageUri);
+
+        final ImageButton imageDiscardButton = findViewById(R.id.image_discard_button);
+        imageDiscardButton.setOnClickListener(v -> discardImage(this, imageUri));
+
+        final ImageButton imageSaveButton = findViewById(R.id.image_save_button);
+        imageSaveButton.setOnClickListener(v -> saveImage(this));
+
+        final ImageButton imageShareButton = findViewById(R.id.share_button);
+        imageShareButton.setOnClickListener(v -> shareImage(this, imageUri));
+    }
+
+    private void discardImage(ImageViewer callingActivity, Uri image) {
+        getContentResolver().delete(image, null, null);
+        callingActivity.finish();
+    }
+
+    private void saveImage(ImageViewer callingActivity) {
+        callingActivity.finish();
+    }
+
+    private void shareImage(ImageViewer callingActivity, Uri image) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_STREAM, image);
+        callingActivity.startActivity(intent);
     }
 }
