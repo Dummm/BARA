47c59a374ac6ba4e40474848f1f0322d63f00d1c
Dummmm <niros98@gmail.com>
2020-05-30 16:34:13 +0300

    Small UI changes

--- app/src/main/java/com/bara/bara/ImageViewer.java
+++ app/src/main/java/com/bara/bara/ImageViewer.java
@@ -1,9 +1,11 @@
 package com.bara.bara;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -16,6 +18,11 @@ public class ImageViewer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
         Intent intent = getIntent();
         String fileName = intent.getStringExtra("filepath");
